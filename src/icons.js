2db3466042c500f9072552b75c623ee5bc23a6b5
Adrian Hanft <adrian3@users.noreply.github.com>
2026-05-28 22:03:50 -0600

    Fix embedded host rendering

---
+++ src/icons.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { Cog6ToothIcon, EyeIcon, EyeSlashIcon, FunnelIcon } from "@heroicons/react/24/outline";
+
+function withIconDefaults(Icon) {
+  return function DropBoardIcon({ className, ...props }) {
+    return <Icon aria-hidden="true" className={className} {...props} />;
+  };
+}
+
+export const DropBoardFilterIcon = withIconDefaults(FunnelIcon);
+export const DropBoardHideIcon = withIconDefaults(EyeSlashIcon);
+export const DropBoardSettingsIcon = withIconDefaults(Cog6ToothIcon);
+export const DropBoardShowIcon = withIconDefaults(EyeIcon);
